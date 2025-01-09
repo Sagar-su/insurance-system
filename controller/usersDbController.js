3f54dd50ba21b744ba1ee9aedef82d25d9c3c7df
Sagar-su <sahil.grewal@infoaxon.com>
2025-01-09 15:55:42 +0530

    Update usersDbController.js

--- controller/usersDbController.js
+++ controller/usersDbController.js
@@ -1,37 +1,37 @@
 
-const {getAllUsers,addUser} =require('../DbModels/usersModel')
-const client = require('../client')
+// const {getAllUsers,addUser} =require('../DbModels/usersModel')
+// const client = require('../client')
 
-const getUsers=async(req,res)=>{
-try{
-        const result= await client.get("userData");
-            if(!result){
+// const getUsers=async(req,res)=>{
+// try{
+//         const result= await client.get("userData");
+//             if(!result){
                 
-                const usersData = await getAllUsers();
-                await client.set("userData",JSON.stringify(usersData,null,2));
-                res.json(usersData)
+//                 const usersData = await getAllUsers();
+//                 await client.set("userData",JSON.stringify(usersData,null,2));
+//                 res.json(usersData)
 
-            }
-            else{
-                res.json(JSON.parse(result));
-            }
+//             }
+//             else{
+//                 res.json(JSON.parse(result));
+//             }
             
       
 
-    }
-    catch(error){
-        res.json(error.message);
+//     }
+//     catch(error){
+//         res.json(error.message);
         
-    }
-// res.json("working")
-}
-const addNewUser= (req,res)=>{
-    const newUserData = req.body;
-    const newUser = addUser(newUserData);
-    res.json(newUser);
+//     }
+// // res.json("working")
+// }
+// const addNewUser= (req,res)=>{
+//     const newUserData = req.body;
+//     const newUser = addUser(newUserData);
+//     res.json(newUser);
 
 
-}
+// }
 
 
 module.exports ={
